6cc582235336542739478733b8a30f39988679e5
liteytdownloader <112754415+liteytdownloader@users.noreply.github.com>
2024-07-24 01:33:01 +0330

    Add files via upload

--- Kodak TV/movies.js
+++ Kodak TV/movies.js
@@ -1,25 +1,14 @@
 var moviesList = {
 	Movies: [
 		{
-			Title: 'قطار دایناسورها',
-			Thumb: 'https://s35.upera.net/s3/posters/ECnVUlzr78jV9OP8JqNj.jpg',
-			Episodes: [
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu', Title: 'جزیره ماجراجویی'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'}
-			]
-		},
-		{
 			Title: 'گارفیلد',
 			Thumb: 'https://s35.upera.net/s3/posters/WnFOLuFcddH1FlELFxOM.jpg',
 			Episodes: [
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu', Title: 'جزیره ماجراجویی'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
-				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'}
+				{hls: 'https://adrin.upera.tv/2783185-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2783184-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2747914-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2972675-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2984234-0-hls.m3u8?ref=w1iu'}			
 			]
 		},
 		{
@@ -47,8 +36,88 @@ var moviesList = {
 				{hls: 'https://adrin.upera.tv/2980037-0-hls.m3u8?ref=w1iu', SE: 4},
 				{hls: 'https://adrin.upera.tv/2980038-0-hls.m3u8?ref=w1iu', SE: 4},
 				{hls: 'https://adrin.upera.tv/2980039-0-hls.m3u8?ref=w1iu', SE: 4},
-				{hls: 'https://adrin.upera.tv/2980040-0-hls.m3u8?ref=w1iu', SE: 4}
+				{hls: 'https://adrin.upera.tv/2980040-0-hls.m3u8?ref=w1iu', SE: 4},	
+				{hls: 'https://adrin.upera.tv/2980020-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980021-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980022-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980023-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980024-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980027-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980025-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980026-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980028-0-hls.m3u8?ref=w1iu', SE: 3},
+				{hls: 'https://adrin.upera.tv/2980029-0-hls.m3u8?ref=w1iu', SE: 3}		
 			]
+		},
+		
+		{
+			Title: 'هات ویلز بیایید مسابقه دهیم',
+			Thumb: 'https://s35.upera.net/s3/posters/D05w84hFPXRzrbsdB3OG.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2984541-0-hls.m3u8?ref=w1iu', SE: 1},
+				{hls: 'https://adrin.upera.tv/2984542-0-hls.m3u8?ref=w1iu', SE: 1},
+				
+			]
+		},
+		
+		{
+			Title: 'تام و جری در نیویورک',
+			Thumb: 'https://s35.upera.net/s3/posters/YIPP4qi6qDMTxmENW3O6.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2909468-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2909469-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2909470-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2909471-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2909472-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2909474-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2909477-0-hls.m3u8?ref=w1iu'}
+			]
+		},
+		
+		{
+			Title: 'تام و جری',
+			Thumb: 'https://s35.upera.net/s3/posters/pwUa3rRadfV5zIsDHWUI.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2748882-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748883-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748973-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748974-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748985-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748986-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748988-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748993-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2748994-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2749023-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2749024-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2749025-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2749026-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2749027-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2749028-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2750113-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2750114-0-hls.m3u8?ref=w1iu'}
+			]
+		},
+		
+		{
+			Title: 'گیسوکمند | راپونزل',
+			Thumb: 'https://s35.upera.net/s3/posters/wFG0MFVMwNtnHmYKenkp.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2748402-0-hls.m3u8?ref=w1iu', Title: 'گیسوکمند'},
+				{"hls":"https://adrin.upera.tv/2776635-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776636-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776637-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776638-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776639-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776640-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776641-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776642-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776643-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776644-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776645-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776646-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776647-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776649-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776653-0-hls.m3u8?ref=w1iu"},
+				{"hls":"https://adrin.upera.tv/2776630-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776631-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776632-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776633-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2776634-0-hls.m3u8?ref=w1iu"}
+			]
+		},
+		
+		{
+			Title: 'قورباغه خنگ',
+			Thumb: 'https://s35.upera.net/s3/posters/qPESoizeom54bQJKlNFx.jpg',
+			Episodes: [{"hls":"https://adrin.upera.tv/2750218-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2750219-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2750220-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2750221-0-hls.m3u8?ref=w1iu"},{"hls":"https://adrin.upera.tv/2750222-0-hls.m3u8?ref=w1iu"}]
+		},
+		
+		{
+			Title: 'بزرگ بچه کوسه',
+			Thumb: 'https://s35.upera.net/s3/posters/uh2VDhWphADHlXOmBAm6.jpg',
+			Episodes: [{"hls":"https://adrin.upera.tv/2944958-0-hls.m3u8?ref=w1iu", Title: 'بزرگ بچه کوسه!'}]
 		}
 	],
 	Updated: 1721738798
