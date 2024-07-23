e9fd016cb6b50caccee0479de24bc69e7394fc7c
liteytdownloader <112754415+liteytdownloader@users.noreply.github.com>
2024-07-23 19:10:40 +0330

    Add files via upload

---
+++ Kodak TV/movies.js
@@ -0,0 +1,55 @@
+var moviesList = {
+	Movies: [
+		{
+			Title: 'قطار دایناسورها',
+			Thumb: 'https://s35.upera.net/s3/posters/ECnVUlzr78jV9OP8JqNj.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu', Title: 'جزیره ماجراجویی'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'}
+			]
+		},
+		{
+			Title: 'گارفیلد',
+			Thumb: 'https://s35.upera.net/s3/posters/WnFOLuFcddH1FlELFxOM.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu', Title: 'جزیره ماجراجویی'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2982187-0-hls.m3u8?ref=w1iu'}
+			]
+		},
+		{
+			Title: 'کمپ اسنوپی',
+			Thumb: 'https://s35.upera.net/s3/posters/Jn1YfldbvknWUWKEpc5r.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2983237-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2983239-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2983240-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2983241-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2983242-0-hls.m3u8?ref=w1iu'},
+				{hls: 'https://adrin.upera.tv/2983243-0-hls.m3u8?ref=w1iu'}
+			]
+		},
+		{
+			Title: 'ماجراهای ابرقهرمانان مارول',
+			Thumb: 'https://s35.upera.net/s3/posters/Qb0BnMMhpdLmmasVr7TB.jpg',
+			Episodes: [
+				{hls: 'https://adrin.upera.tv/2980009-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980032-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980033-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980034-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980035-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980036-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980037-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980038-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980039-0-hls.m3u8?ref=w1iu', SE: 4},
+				{hls: 'https://adrin.upera.tv/2980040-0-hls.m3u8?ref=w1iu', SE: 4}
+			]
+		}
+	],
+	Updated: 1721738798
+};
